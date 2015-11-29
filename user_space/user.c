d037f09b02c783a10dffad595afc4746230d400a
Taehwan Kim <maxtortime@gmail.com>
2015-11-29 11:15:21 +0900

    user illegal seek 에러 발생

--- user_space/user.c
+++ user_space/user.c
@@ -3,11 +3,15 @@
 
 #define SIZE 200
 
+enum COMMANDS {ENQUEUE=1, DEQUEUE, PRINT, QUIT};
+
 int main() {
     char msg[SIZE];
+    char buffer[SIZE];
+
     int commands, number;
     
-    FILE *fp = fopen("/dev/cdev_example","r");
+    FILE *fp = fopen("/dev/cdev_example","r+");
     
     printf("Welcome to user-kernel interacting program!..\n");
 
@@ -16,19 +20,37 @@ int main() {
         exit(1);
     }
     else {
-		
-		printf("Opening character driver device is sucessful!\n");
-		while(commands!=4) {	
+		printf("Opening character driver device is \
+			sucessful!\n");
+
+		while(commands!=QUIT) {	
 			printf("1) Enqueue 2) Dequeue 3) Print 4) quit > ");
 			scanf("%d",&commands);
 			
-			if (commands==1) {
-				printf("Please input integer...> ");
-				scanf("%d",&number);
+			switch(commands) {
+				case ENQUEUE:
+					printf("Please input integer...> ");
+					scanf("%d",&number);
 				
-				sprintf(msg,"enqueue %d",number);
-				fputs(msg,fp);
-
+					sprintf(msg,"enqueue %d",number);
+				
+					fprintf(fp,"%s",msg);
+					fgets(buffer,SIZE,fp);
+					printf("%s",buffer);
+					break;
+				case DEQUEUE:
+					fprintf(fp,"dequeue");
+					fgets(buffer,SIZE,fp);
+					printf("%s",buffer);
+					break;
+				case PRINT:
+					fprintf(fp,"print");
+					fgets(buffer,SIZE,fp);
+					printf("%s",buffer);
+					break;
+				default:
+					perror("Error: ");
+					exit(1);
 			}
 		}		
     }
